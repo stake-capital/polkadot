96d41aee391d6dd7f03c5360347e17b86d7492e4
Julien Bouteloup <julien.bouteloup@gmail.com>
2019-08-01 16:06:22 +0200

    update source $HOME/.cargo/env
    updating file while running commands

--- kusama_install.sh
+++ kusama_install.sh
@@ -4,6 +4,7 @@
 sudo apt update
 sudo apt upgrade -y
 curl https://sh.rustup.rs -sSf | sh
+source $HOME/.cargo/env
 rustup update
 sudo apt install make clang pkg-config libssl-dev build-essential
 
